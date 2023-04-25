ba29354e02eeddc7f0a7e6b36c2a935c3d73b9dd
Ermiyas <ermiyas.dagnachew@gmail.com>
2023-04-25 15:38:52 +0300

    Added Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button, GridItem, Show } from "@chakra-ui/react";
+import { GridItem, Show } from "@chakra-ui/react";
 import { Grid } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 function App() {
@@ -6,17 +6,13 @@ function App() {
     <Grid
       templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area="nav" bg="coral">
+      <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
+        <GridItem area="aside">Aside</GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
+      <GridItem area="main">Main</GridItem>
     </Grid>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import icon from "../assets/User Game.ico";
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={icon} boxSize="60px" />
+      <Text>A Navigation Bar</Text>
     </HStack>
   );
 };
